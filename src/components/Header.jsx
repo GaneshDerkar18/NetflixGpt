7220c14ac51b7230689096121e5a7a615930ba62
18grd <ganesh.derkar.dev@gmail.com>
2025-04-30 22:26:07 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <img src="../../images/Netflix_logo_PMS.png" alt="" srcset="" />
+    </div>
+  )
+}
+
+export default Header;
